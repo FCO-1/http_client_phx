e9c8dbee290e2432d7231c13f65f12a86ac23b72
FCO-1 <pacomendez012@gmail.com>
2025-04-18 12:35:29 -0600

    adiciones sobre router

--- lib/http_client_phx_web/router.ex
+++ lib/http_client_phx_web/router.ex
@@ -1,6 +1,10 @@
 defmodule HttpClientPhxWeb.Router do
   use HttpClientPhxWeb, :router
 
+  # Define un pipeline llamado :browser que se utiliza para manejar solicitudes
+  # HTML. Incluye plugs para aceptar solicitudes HTML, manejar sesiones,
+  # mostrar mensajes flash en vivo, establecer un diseño raíz, proteger contra
+  # ataques CSRF y configurar encabezados de seguridad.
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -10,49 +14,52 @@ defmodule HttpClientPhxWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  # Define un pipeline llamado :api que se utiliza para manejar solicitudes JSON.
+  # Este pipeline es útil para construir APIs.
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  # Define un scope para las rutas principales de la aplicación.
+  # Usa el pipeline :browser para manejar estas rutas.
   scope "/", HttpClientPhxWeb do
     pipe_through :browser
 
+    # Define una ruta GET para la raíz del sitio que apunta al controlador PageController
+    # y a la acción :index.
     get "/", PageController, :index
-    live "/request", RequestLive, :request
+
+    # Define una ruta para manejar LiveViews en "/request", apuntando al módulo
+    # RequestLive.Index y a la acción :index.
+    live "/request", RequestLive.Index, :index
   end
 
-
-
-  # Other scopes may use custom stacks.
+  # Sección comentada para definir un scope personalizado para APIs.
+  # Puedes descomentar y personalizar esta sección si necesitas manejar rutas de API.
   # scope "/api", HttpClientPhxWeb do
   #   pipe_through :api
   # end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
+  # Habilita el LiveDashboard solo en entornos de desarrollo y prueba.
+  # El LiveDashboard proporciona métricas y herramientas de depuración para Phoenix.
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
       pipe_through :browser
 
+      # Define una ruta para el LiveDashboard en "/dashboard".
       live_dashboard "/dashboard", metrics: HttpClientPhxWeb.Telemetry
     end
   end
 
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
+  # Habilita la vista previa del buzón de Swoosh en desarrollo.
+  # Esto permite ver los correos electrónicos enviados por la aplicación en un navegador.
   if Mix.env() == :dev do
     scope "/dev" do
       pipe_through :browser
 
+      # Define una ruta para la vista previa del buzón en "/dev/mailbox".
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
